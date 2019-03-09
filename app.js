4ba5823c21ed7b19464e7941be40db4214ae93d8
moffatarap <arapaoa.moffat@gmail.com>
2019-03-09 14:23:56 +1300

    3.9.19 - BotStart
    Bot starting point.

---
+++ app.js
@@ -0,0 +1,9 @@
+const Telegraf = require('telegraf') // import telegraf framework
+require('dotenv').config() //Allows for the use of bot token in a env file
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start((ctx) => ctx.reply('Welcome!'))
+bot.help((ctx) => ctx.reply('Send me a sticker'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+bot.launch()
