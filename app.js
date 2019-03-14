804b924accc2cbbf56cef7d3fa7f4f09772ca0c9
moffatarap <arapaoa.moffat@gmail.com>
2019-03-14 20:23:06 +1300

    03.14.19 - Push
    Push before today updates.

--- app.js
+++ app.js
@@ -2,17 +2,46 @@
 //FRAMEWORKS
 const Telegraf = require('telegraf') // import telegraf framework
 var request = require('request'); //uses request API for getting JSON
+var $ = require('jQuery');
+const jsdom = require("jsdom");
 require('dotenv').config() //Allows for the use of bot token in a env file
 
 //VAR
 var geonetVolcano = "https://api.geonet.org.nz/volcano/val"; //saves goenet url as var
+var geonetVolcanoDataJSON;
+var volcanoDataDisplay; 
 const bot = new Telegraf(process.env.BOT_TOKEN) //bot token in .env file
 const trigger = 'volcano';
+
+//ARRAY
+var volcanoMarkerTitleArray;
+var volcanoLevelArray;
+var volcanoActivityArray;
+var volcanoHazardsArray;
+var volcanonLatArray;
+var volcanonLngArray;
 
 
 /* Geonet Data Get */
 function GeonetDataVol() {
-    console.log('test');
+    console.log('GeonetDatVol_Called');
+   /* $.getJSON(geonetVolcano, function (data) {
+        $.each(data.features, function (i, vol) {
+             //data id displayed in table row || this one is volcano title
+             if (i < volTitleLength) {
+                 volcanoMarkerTitleArray[i] = vol.properties.volcanoTitle;
+                 volcanoLevelArray[i] = vol.properties.level;
+                 volcanoActivityArray[i] = vol.properties.activity;
+                 volcanoHazardsArray[i] = vol.properties.hazards;
+                 volcanonLatArray[i] = vol.geometry.coordinates[1]; //access first element
+                 volcanonLngArray[i] = vol.geometry.coordinates[0]; //access second element
+                 i++;
+             }
+             else {
+                 
+             }
+         });
+ }); */
 }
 
 
@@ -23,7 +52,7 @@ bot.start((ctx) => ctx.reply('Welcome to Disaster Zone Chatbot! I report on eart
 bot.hears(trigger, (ctx) => {
 GeonetDataVol();
 console.log('VOLCANO');
-return ctx.reply(`${geonetVolcano}`)
+return ctx.reply(`${geonetVolcanoDataJSON}`)
 
 })
 
